00086bba34944ca67632a7b46bbd5e4a6578d10c
faust6669 <123564743+faust6669@users.noreply.github.com>
2025-07-18 17:46:17 -0400

    Update favicon and related icons

--- Landing-page/src/components/Hero.js
+++ Landing-page/src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
     <div className="bg-light p-5 rounded-lg m-3">
       <Container>
         <h1 className="display-4">Welcome to my page</h1>
-        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.Drake is a noob</p>
         <hr className="my-4" />
         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
         <p className="lead">
